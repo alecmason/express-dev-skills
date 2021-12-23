cc8d2b23ddfecf1d538f4f36be3566932e15044e
Alec Jones <alecjones@Alecs-MacBook-Air.local>
2021-12-22 18:11:58 -0600

    lab part 2 finish

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,22 +1,56 @@
-var Skill = require('../models/skills');
+var Skill = require("../models/skills");
 
 module.exports = {
-    index,
-    show,
-
+  index,
+  show,
+  new: newSkill,
+  create,
+  delete: deleteSkill,
+  edit,
+  update,
 };
 
 function index(req, res) {
-    res.render('skills/index', {
-        skills: Skill.getAll(),
-        time: req.time
-    });
+  res.render("skills/index", {
+    skills: Skill.getAll(),
+    time: req.time,
+  });
 }
 
 function show(req, res) {
-    res.render('skills/show', {
-        skill: Skill.getOne(req.params.id),
-        // Would like to display the number of the todo within the list
-        skillNum: Skill.getAll().findIndex(skill => skill.id === parseInt(req.params.id)) + 1
-    });
+  res.render("skills/show", {
+    skill: Skill.getOne(req.params.id),
+    skillNum:
+      Skill.getAll().findIndex(
+        (skill) => skill.id === parseInt(req.params.id)
+      ) + 1,
+  });
 }
+
+function newSkill(req, res) {
+  res.render("skills/new");
+}
+
+function create(req, res) {
+  console.log(req.body);
+  req.body.done = false;
+  Skill.create(req.body);
+  res.redirect("/skills");
+}
+
+function update(req, res) {
+  req.body.done = req.body.done === "on";
+  Todo.update(req.params.id, req.body);
+  res.redirect("/skills");
+}
+
+function edit(req, res) {
+  res.render("skills/edit", {
+    skill: Skill.getOne(req.params.id),
+  });
+}
+
+function deleteSkill(req, res) {
+    Skill.deleteOne(req.params.id);
+    res.redirect('/skills');
+  }
--- models/skills.js
+++ models/skills.js
@@ -1,21 +1,41 @@
 // The ids can be generated using:
 // Date.now() % 1000000
 const skills = [
-    {id: 125223, skill: 'Javascript', ability: true},
-    {id: 127904, skill: 'ExpressJS', ability: false},
-    {id: 139608, skill: 'NodeJS', ability: false},
-  ];
-  
-  module.exports = {
-    getAll,
-    getOne,
-  };
-    
-  function getOne(id) {
-    // Use the Array.prototype.find iterator method
-    return skills.find(skill => skill.id === parseInt(id));
-  }
-  
-  function getAll() {
-    return skills;
-  }
+  { id: 125223, skill: "Javascript", ability: true },
+  { id: 127904, skill: "ExpressJS", ability: false },
+  { id: 139608, skill: "NodeJS", ability: false },
+];
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  deleteOne
+};
+
+function getOne(id) {
+  // Use the Array.prototype.find iterator method
+  return skills.find((skill) => skill.id === parseInt(id));
+}
+
+function getAll() {
+  return skills;
+}
+
+function create(skill) {
+  skill.id = Date.now() % 1000000;
+  skill.done = false;
+  skills.push(skill);
+}
+
+function update(id, skill) {
+  const idx = skills.findIndex((skill) => skill.id === parseInt(id));
+  skill.id = parseInt(id);
+  skills.splice(idx, 1, skill);
+}
+
+function deleteOne(id) {
+  const idx = skills.findIndex((skill) => skill.id === parseInt(id));
+  skills.splice(idx, 1);
+}
